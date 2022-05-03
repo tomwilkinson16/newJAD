0ae4def9b26ca239cdf6ffde4337cde158c864a8
tomwi <tomwi@LAPTOP-J6CO8BPI>
2022-05-03 13:29:13 +0100

    Button clean up and JavaDoc

--- JAD_ICA-master/src/main/java/GUI/ButtonPanel.java
+++ JAD_ICA-master/src/main/java/GUI/ButtonPanel.java
@@ -7,13 +7,16 @@ import Interfaces.SummaryInterface;
 import javax.swing.JFileChooser;
 
 /**
+ * This class is dedicated to the functionality of the GUI; adding chairs,
+ * tables desks as well as serializing the data, clearing and totalling the
+ * basket, and finally displaying the basket to the console.
  *
  * @author tomwi
  */
-public class ButtonPanel extends javax.swing.JPanel{
+public class ButtonPanel extends javax.swing.JPanel {
 
     /**
-     * Creates new form ButtonPanel
+     * Creates new form ButtonPanel using the initComponenets method.
      */
     public ButtonPanel() {
         initComponents();
@@ -124,52 +127,144 @@ public class ButtonPanel extends javax.swing.JPanel{
         add(showSummaryButton);
     }// </editor-fold>//GEN-END:initComponents
 
+    /**
+     * This constructor is the implementation of the ItemInterface and the
+     * listener watches for changes to the state of the object.
+     *
+     * @param listener
+     */
+    public void addItemlListener(ItemInterface listener) {
+        this.itemListener = listener;
+    }
+
+    /**
+     * This constructor is the implementation of the BasketInterface and the
+     * listener watches for changes to the state of the object.
+     *
+     * @param listener
+     */
+    public void addBasketListener(BasketInterface listener) {
+        this.basketListener = listener;
+    }
+
+    /**
+     * This constructor is the implementation of the SummaryInterface and the
+     * listener watches for changes to the state of the object.
+     *
+     * @param listener
+     */
+    public void addSummaryListener(SummaryInterface listener) {
+        this.summaryListener = listener;
+    }
+
+    /**
+     * This constructor is the implementation of the serializableInterface and
+     * the listener watches for changes to the state of the object.
+     *
+     * @param listener
+     */
+    public void setSerializeListener(SerializationInterface listener) {
+        this.serializeListener = listener;
+    }
+
+    /**
+     * This method uses the itemListener method from the itemInterface, this
+     * listens to chair panel to update the state of the object.
+     *
+     * @param evt
+     */
     private void addChairButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addChairButtonActionPerformed
-        
+
         itemListener.newChairPanel();
     }//GEN-LAST:event_addChairButtonActionPerformed
 
+    /**
+     * This method uses the itemListener method from the itemInterface, this
+     * listens to table panel to update the state of the object.
+     *
+     * @param evt
+     */
     private void addTableButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addTableButtonActionPerformed
-        
+
         itemListener.newTablePanel();
     }//GEN-LAST:event_addTableButtonActionPerformed
 
+    /**
+     * This method uses the itemListener method from the itemInterface, this
+     * listens to desk panel to update the state of the object.
+     *
+     * @param evt
+     */
     private void addDeskButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addDeskButtonActionPerformed
 
         itemListener.newDeskPanel();
     }//GEN-LAST:event_addDeskButtonActionPerformed
 
+    /**
+     * This method uses the summary method from the summaryInterface, this
+     * listens to all the panels and displays the summary of the objects to
+     * console.
+     *
+     * @param evt
+     */
     private void showSummaryButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_showSummaryButtonActionPerformed
-        
+
         summaryListener.showBasketSummary();
-        
+
     }//GEN-LAST:event_showSummaryButtonActionPerformed
 
+    /**
+     * This method uses the removeAllItemsFromBasket from the basketInterface ,
+     * this listens to all the panels and removes all objects from the basket
+     *
+     * @param evt
+     */
     private void clearBasketButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearBasketButtonActionPerformed
-        
+
         basketListener.removeAllItemsFromBasket();
     }//GEN-LAST:event_clearBasketButtonActionPerformed
 
+    /**
+     * This method uses the totalPricePanel and itemListener to access the total
+     * price of the basket,once the button is pressed it displays the total
+     * price to the GUI.
+     *
+     * @param evt
+     */
     private void totalPriceButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_totalPriceButtonActionPerformed
-        
+
         itemListener.totalPricePanel();
     }//GEN-LAST:event_totalPriceButtonActionPerformed
 
+    /**
+     * This method loads the basket, it uses JFileChooser to bring a window up
+     * for the user to choose which file they would like to load, if the file is
+     * a valid file, the basket will be loaded and the GUI will be updated.
+     *
+     * @param evt
+     */
     private void loadBasketButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loadBasketButtonActionPerformed
-        
+
         int choice = 0;
         JFileChooser file = new JFileChooser();
         choice = file.showOpenDialog(this);
-        if (choice == JFileChooser.APPROVE_OPTION){
-            serializeListener.loadBasket(file.getSelectedFile());            
-        }    
+        if (choice == JFileChooser.APPROVE_OPTION) {
+            serializeListener.loadBasket(file.getSelectedFile());
+        }
     }//GEN-LAST:event_loadBasketButtonActionPerformed
 
+    /**
+     * This method saves the basket, it uses JFileChooser to bring a window up
+     * for the user to input a correct file name and file type, if the file is a
+     * valid file, the basket will be saved and the GUI will be updated.
+     *
+     * @param evt
+     */
     private void saveBasketButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveBasketButtonActionPerformed
         int choice = 0;
         JFileChooser file = new JFileChooser();
         choice = file.showSaveDialog(this);
-        if (choice == JFileChooser.APPROVE_OPTION){
+        if (choice == JFileChooser.APPROVE_OPTION) {
             serializeListener.saveBasket(file.getSelectedFile());
         }
     }//GEN-LAST:event_saveBasketButtonActionPerformed
@@ -198,22 +293,4 @@ public class ButtonPanel extends javax.swing.JPanel{
     private BasketInterface basketListener;
     private SummaryInterface summaryListener;
     private SerializationInterface serializeListener;
-
-    
-    
-    public void addItemlListener(ItemInterface listener) {
-        this.itemListener = listener;
-    }
-    
-    public void addBasketListener(BasketInterface listener) {
-        this.basketListener = listener;
-    }
-    
-    public void addSummaryListener(SummaryInterface listener) {
-        this.summaryListener = listener;
-    }    
-    
-    public void setSerializeListener(SerializationInterface listener){
-        this.serializeListener = listener;
-    }
 }
